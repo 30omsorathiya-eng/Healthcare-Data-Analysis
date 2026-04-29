f01321e067de2f0cc8b611f08ea8c3b13fbc5b20
30omsorathiya-eng <30omsorathiya@gmail.com>
2026-04-29 13:46:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Healthcare Data Analysis Dashboard")
+
+st.write("Welcome to my Data Analyst Project")
+
+st.header("Project Overview")
+st.write("This project analyzes healthcare data using Python, Tableau, and visualization tools.")
+
+st.header("Insights")
+st.write("• Patient trends analysis")
+st.write("• Healthcare system load")
+st.write("• Data-driven decision making")
+
+st.success("Project successfully deployed using Streamlit 🚀")
